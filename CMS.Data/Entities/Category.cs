cf2dcb442a63009f309f2f8e2e2464fb0a4285c8
Pham Dang Khoa <khoacfvn3@gmail.com>
2026-05-14 13:03:06 +0700

    Add project files.

---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Sinh viên: Phạm Đăng Khoa
+ * Mã Sinh Viên : 2123110058
+ * Version 1.0
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    //thự thể danh mục bài viết
+    public class Category
+    {
+        public int Id { get; set; } //mã danh mục bài viết, khoá chính
+
+        public string Name { get; set; } = string.Empty; //Tên danh mục (vd: Tin Giáo Dục)
+
+        public string Description { get; set; } //mô tả
+
+        // Quan hệ: Một danh mục có nhiều bài viết
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ CMS.Data/Entities/CategoryProduct.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Sinh viên: Phạm Đăng Khoa
+ * Mã Sinh Viên : 2123110058
+ * Version 1.0
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class CategoryProduct
+    {
+    }
+}
